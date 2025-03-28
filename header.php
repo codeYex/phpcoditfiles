67ff47b0754e9894bec8271089b5bda9d17a5b91
codeYex <yextheprogrammer@gmail.com>
2025-03-28 09:06:47 +0300

    Updated code

--- header.php
+++ header.php
@@ -1,39 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
 
-
- <!DOCTYPE html>
- <html lang="en">
- <head>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <title>Nav</title>
- </head>
- <body>
- <?php
+</head>
 
-//page URLS
-$pageUrls = [
-    'Home Page' => './index.php',
-    'variables' => './variables.php',
-    'conditionals' => './conditionals.php',
-    'foreach' => './for_loops.php',
-];
+<body>
+    <?php
+    //page URLS
+    $pageUrls = [
+        'Home Page' => [
+            'url' => './index.php',
+            'icon'=> 'fa-solid fa-house'
+        ],
+        'Variables' => [
+            'url' => './variables.php'
+        ]
 
-//Function to generate navigation Links
-function generateUrlLinks($url, $label)
-{
-    $isActive = basename($_SERVER['PHP_SELF']) == basename($url) ? 'class = "Active" ' : '';
-    return "<a href=\"$url\" $isActive>$label</a>";
-}
-?>
-<nav class="dashboard_nav">
- <?php
-    echo generateUrlLinks($pageUrls['Home Page'], 'Home Page');
-    echo generateUrlLinks($pageUrls['variables'], 'Variables');
-    echo generateUrlLinks($pageUrls['foreach'], 'Loops');
-    echo generateUrlLinks($pageUrls['conditionals'], 'conditionals');
+        ];
+    $pageUrls = [
+        'Home Page' => './index.php',
+        'variables' => './variables.php',
+        'conditionals' => './conditionals.php',
+        'foreach' => './for_loops.php',
+    ];
 
+    //Function to generate navigation Links
+    function generateUrlLinks($url, $label)
+    {
+        $isActive = basename($_SERVER['PHP_SELF']) == basename($url) ? 'class = "Active" ' : '';
+        return "<a href=\"$url\" $isActive>$label</a>";
+    }
     ?>
-</nav>
- </body>
- </html>
+    <nav class="dashboard_nav">
+        <?php
+        echo generateUrlLinks($pageUrls['Home Page'], 'Home Page');
+        echo generateUrlLinks($pageUrls['variables'], 'Variables');
+        echo generateUrlLinks($pageUrls['foreach'], 'Loops');
+        echo generateUrlLinks($pageUrls['conditionals'], 'conditionals');
+        ?>
+    </nav>
+</body>
+</html>
--- variables.php
+++ variables.php
@@ -1,4 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./app.css">
+    <title>Variables</title>
+</head>
+<body>
 <?php  
-
-
 include 'header.php';
+
+echo "-------CALCULATING AREA --------"; 
+echo "<br>";
+
+$width = 23;
+$length = 54;
+$area = $length * $width;
+echo "The area of the land is : " . $area . " Square meters";
+
+?>
+</body>
+</html>
