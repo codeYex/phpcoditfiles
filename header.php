7d0ebed9d9fb1eab726fa1aa4566c3e2aab0b569
codeYex <yextheprogrammer@gmail.com>
2025-03-29 11:23:55 +0300

    Updated the header file to include icons on nav links

--- header.php
+++ header.php
@@ -6,43 +6,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="app.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <title>Nav</title>
+    <title>Navigation </title>
 </head>
 
 <body>
     <?php
     //page URLS
     $pageUrls = [
-        'Home Page' => [
+        'Home_Page' => [
             'url' => './index.php',
-            'icon'=> 'fa-solid fa-house'
+            'icon' => 'fa-solid fa-house'
         ],
-        'Variables' => [
-            'url' => './variables.php'
+        'variables' => [
+            'url' => './variables.php',
+            'icon' => 'fa-solid fa-code',
+        ],
+        'Conditionals' => [
+            'url' => './conditionals.php',
+            'icon' => 'fa-solid fa-arrows-split-up-and-left',
+        ],
+        'for_loops' => [
+            'url' => './for_loops.php',
+            'icon' => 'fa-solid fa-repeat',
         ]
 
-        ];
-    $pageUrls = [
-        'Home Page' => './index.php',
-        'variables' => './variables.php',
-        'conditionals' => './conditionals.php',
-        'foreach' => './for_loops.php',
     ];
-
     //Function to generate navigation Links
-    function generateUrlLinks($url, $label)
+    function generateUrlLinks($pageInfo)
     {
-        $isActive = basename($_SERVER['PHP_SELF']) == basename($url) ? 'class = "Active" ' : '';
-        return "<a href=\"$url\" $isActive>$label</a>";
+
+        $url = $pageInfo['url'];
+        $icon = $pageInfo['icon'];
+        $label = ucfirst(str_replace('_', '', basename($url, '.php')));
+        $isActive = basename($_SERVER['PHP_SELF']) == basename($url) ? 'class = "Active"' : '';
+
+        return "<a href=\"$url\" $isActive>
+        <i class=\"$icon\"></i>
+        $label
+    </a>";
     }
     ?>
     <nav class="dashboard_nav">
         <?php
-        echo generateUrlLinks($pageUrls['Home Page'], 'Home Page');
-        echo generateUrlLinks($pageUrls['variables'], 'Variables');
-        echo generateUrlLinks($pageUrls['foreach'], 'Loops');
-        echo generateUrlLinks($pageUrls['conditionals'], 'conditionals');
+        echo generateUrlLinks($pageUrls['Home_Page']);
+        echo generateUrlLinks($pageUrls['Conditionals']);
+        echo generateUrlLinks($pageUrls['for_loops']);
+        echo generateUrlLinks($pageUrls['variables']);
+
         ?>
     </nav>
 </body>
+
 </html>
